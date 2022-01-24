a2586690d72388f44333974059815e2455b7f7ab
Carmendemaicena <95417797+Carmendemaicena@users.noreply.github.com>
2022-01-24 21:08:11 +0100

    Add files via upload

---
+++ exercici5/function.c
@@ -0,0 +1,22 @@
+/*
+ * function.c
+ *
+ *  Created on: 24 ene 2022
+ *      Author: nil
+ */
+int llegirnum(char *msg){
+    int num;
+    printf("%s", msg);
+    scanf("%d", &num);
+    return num;
+}
+
+void canviar(int num,int num2){
+	int aux;
+	aux=num;
+	num=num2;
+	num2=aux;
+	printf("El primer valor era %d, ahora es %d\n",num2,num);
+	printf("El segundo valor era %d, ahora es %d\n",num,num2);
+}
+
---
+++ exercici5/main.c
@@ -0,0 +1,16 @@
+/*
+ * main.c
+ *
+ *  Created on: 24 ene 2022
+ *      Author: nil
+ */
+
+#include <stdio.h>
+#include "function.h"
+
+void main(){
+	int num, num2;
+	num = llegirnum("Coloca el primer valor:\n");
+	num2 = llegirnum("Coloca el segundo valor:\n");
+	canviar(num,num2);
+}
