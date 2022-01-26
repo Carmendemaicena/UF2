b283e4738b504bb9eed11d7037569b8dde566d74
Carmendemaicena <95417797+Carmendemaicena@users.noreply.github.com>
2022-01-26 14:23:48 +0100

    Add files via upload

---
+++ exercici7/function.h
@@ -0,0 +1,17 @@
+/*
+ * function.h
+ *
+ *  Created on: 24 ene 2022
+ *      Author: nil
+ */
+
+#ifndef FUNCTION_H_
+#define FUNCTION_H_
+
+
+
+#endif /* FUNCTION_H_ */
+int llegirnum(char *);
+int getcalues(int *,int);
+void calculate(int *,int);
+
---
+++ exercici7/main.c
@@ -0,0 +1,18 @@
+/*
+ * main.c
+ *
+ *  Created on: 24 ene 2022
+ *      Author: nil
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "function.h"
+
+void main(){
+	int num;
+	num = llegirnum("Coloca el tamany de l'array:\n");
+	int vector[num];
+	getvalues(vector,num);
+	calculate(vector,num);
+}
