a4435626e14fb72100451b0544cee524be385bb4
Carmendemaicena <95417797+Carmendemaicena@users.noreply.github.com>
2022-02-03 17:00:23 +0100

    Add files via upload

---
+++ exercici8/function.h
@@ -0,0 +1,14 @@
+/*
+ * function.h
+ *
+ *  Created on: 2 feb 2022
+ *      Author: nil
+ */
+#ifndef FUNCTION_H_
+#define FUNCTION_H_
+
+
+int value();
+int value2();
+void elevate(num1, num2);
+#endif /* FUNCTIONS_H_ */
---
+++ exercici8/main.c
@@ -0,0 +1,15 @@
+/*
+ * main.c
+ *
+ *  Created on: 2 feb 2022
+ *      Author: nil
+ */
+#include <stdio.h>
+#include "function.h"
+
+void main(){
+	int num1, num2;
+	num1 = value();
+	num2 = value2();
+	elevate(num1, num2);
+}
